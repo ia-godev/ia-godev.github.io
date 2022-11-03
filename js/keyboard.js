42bd4ef65c7ed02a20f9c8226c359941e962b3d8
Iago <iago.alligier@gmail.com>
2022-11-03 20:45:07 +0100

    New style

--- js/keyboard.js
+++ js/keyboard.js
@@ -3,21 +3,21 @@ var phrase = "Le chat.";
 var tps = 0;
 
 function myInput() {
-    var myInput = document.getElementById("myInput");
-    var myOutput = myInput.value;
-    setTimeout(fin, 5000);
-    if (myOutput == phrase) {
-        myInput.disabled = true;
-        myInput.innerHTML = "Tu fais " + tps + "tps";
-    }
+  var myInput = document.getElementById("myInput");
+  var myOutput = myInput.value;
+  setTimeout(fin, 5000);
+  if (myOutput == phrase) {
+    myInput.disabled = true;
+    myInput.innerHTML = "You do " + tps + "tps";
+  }
 }
 
 function restart() {
-    var myInput = document.getElementById("myInput");
-    myInput.disabled = disabled;
+  var myInput = document.getElementById("myInput");
+  myInput.disabled = disabled;
 }
 
 function onLoad() {
-    var phraseP = document.getElementById("phrase");
-    phraseP.innerHTML = phrase;
+  var phraseP = document.getElementById("phrase");
+  phraseP.innerHTML = phrase;
 }
--- js/mouse.js
+++ js/mouse.js
@@ -2,27 +2,27 @@ var clickNumber = 0;
 var timerC = 0;
 
 function clickNm() {
-	timerC = timerC + 1;
-	var clickBtn = document.getElementById("clickBtn");
-	console.log(clickBtn)
-    clickBtn.innerHTML = ++clickNumber;
+  timerC = timerC + 1;
+  var clickBtn = document.getElementById("clickBtn");
+  console.log(clickBtn);
+  clickBtn.innerHTML = ++clickNumber;
 }
 
 function restartNm() {
-	var clickBtn = document.getElementById("clickBtn");
-    clickBtn.innerHTML = clickNumber = 0;
-	clickBtn.disabled = false;
-	timerC = 0;
+  var clickBtn = document.getElementById("clickBtn");
+  clickBtn.innerHTML = clickNumber = 0;
+  clickBtn.disabled = false;
+  timerC = 0;
 }
 
 function ifTimer() {
-	if (timerC == 1) {
-		setTimeout(fin, 10000);
-	}
+  if (timerC == 1) {
+    setTimeout(fin, 10000);
+  }
 }
 
 function fin() {
-	var clickBtn = document.getElementById("clickBtn");
-	clickBtn.innerHTML = "Tu fais " + clickNumber/10 + " cps";
-	clickBtn.disabled = true;
+  var clickBtn = document.getElementById("clickBtn");
+  clickBtn.innerHTML = "You do " + clickNumber / 10 + " cps";
+  clickBtn.disabled = true;
 }
